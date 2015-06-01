9b801ffb4e71083dfc3d612ec45ed038fd457d1e
jamesspargo <jspargo@thoughtworks.com>
2015-05-31 22:04:19 -0500

    JS - integrate the apiParser into the app and create a new node for each api model file.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
+var apiParser = require('./apiParser.js');
+
 var app = {
     buildNodes : function(doc) {
+
         var wrapperDiv = doc.getElementById('wrapper');
-        wrapperDiv.innerHTML = '<div>foo</div>';
-    }
+
+        var apiModels = apiParser.getApiModels();
+        for (apiModel in apiModels) {
+            wrapperDiv.innerHTML += '<div>foo</div>';
+        }
+    },
 }
 
 module.exports = app;
--- test/unit/appSpec.js
+++ test/unit/appSpec.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 var should = require('should');
 var sinon = require('sinon');
 var jsdom = require('jsdom').jsdom;
+
 var app = require('../../js/app.js');
 
 describe('app', function() {
@@ -11,14 +12,20 @@ describe('app', function() {
             assert.equal(typeof app.buildNodes, 'function');
         });
 
-        it('should add a node to the wrapper element', function() {
-
+        it('should add a node to the wrapper element for every api model found by the apiParser', function() {
             var stubbedDiv = "<div id='wrapper'></div>";
-            var document = jsdom(stubbedDiv);
+            var doc = jsdom(stubbedDiv);
 
-            app.buildNodes(document);
-            var actualDiv = document.getElementById('wrapper').innerHTML;
-            var expectedDiv = "<div>foo</div>";
+            var mockedFooJSON = { name: 'foo', url: 'http://example.com', type: 'GET' };
+            var mockedSpargonautJSON = { name: 'spargonaut', url: 'http://spargonaut.com', type: 'GET' };
+            var mockedModelArray = [mockedFooJSON, mockedSpargonautJSON];
+
+            var apiParser = require('../../js/apiParser.js');
+            sinon.stub(apiParser, 'getApiModels').returns(mockedModelArray);
+
+            app.buildNodes(doc);
+            var actualDiv = doc.getElementById('wrapper').innerHTML;
+            var expectedDiv = "<div>foo</div><div>foo</div>";
 
             actualDiv.should.eql(expectedDiv);
         });
