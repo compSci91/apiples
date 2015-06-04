c7391aa344bce72f5949b0d15eaab5a5702f64dc
jamesspargo <jspargo@thoughtworks.com>
2015-06-04 18:22:30 -0500

    JS - implement browserify in the grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-browserify');
+
+    grunt.registerTask('default', 'mochaTest');
 
     grunt.initConfig({
         mochaTest : {
@@ -18,9 +20,15 @@ module.exports = function(grunt) {
                 },
                 src : ['test/**/*.js']
             }
+        },
+
+        browserify : {
+            main : {
+                src : 'src/js/*.js',
+                dest : './bundle.js'
+            }
         }
     });
 
-    grunt.registerTask('default', 'mochaTest');
 
 };
