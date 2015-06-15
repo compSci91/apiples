e844ab81478c73f882c3832f70895decdede5eec
jamesspargo <jspargo@thoughtworks.com>
2015-06-15 00:05:08 -0500

    JS - add a click function to start the scheduled requests;

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 var app = require('./app.js');
 
+var callFrequencyMinutes = 1;
+
 console.log('starting up');
 app.buildNodes(document);
+
+startTimedAction = function () {
+    console.log('starting the timed actions');
+    app.buildScheduledRequests(callFrequencyMinutes, document);
+};
