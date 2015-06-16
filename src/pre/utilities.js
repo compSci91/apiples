1b978b9a12973332e6d59a417653891ab32955fa
scooby-dee <fake@fake.com>
2015-06-16 14:25:41 -0500

    James | Josh Howell - Delete utilities.js

--- src/pre/utilities.js
+++
@@ -1,9 +0,0 @@
-var utilities = {
-    getTopLevelDomainNameFrom : function(url) {
-        var domainPieces = url.split('//');
-        var topDomain = domainPieces[1].split('.')[0];
-       return topDomain;
-    }
-};
-
-module.exports = utilities;
